47c9aba929dbc6018613a5c891502ac25b62683d
Dan Der Flieger <dan.der.flieger@gmail.com>
2025-09-07 15:38:45 -0400

    Kept playing around with values. You can now add any number of "notches" with angle values (within reason - 5 notches is probably the max before pixels get overwritten). Good place to stop.

--- sonex_flap_controller/sonex_flap_controller.ino
+++ sonex_flap_controller/sonex_flap_controller.ino
@@ -29,11 +29,17 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-int ANGLE0 = 0;
-int ANGLE1 = 10;
-int ANGLE2 = 20;
-int ANGLE3 = 30;
+// int ANGLE0 = 0;
+// int ANGLE1 = 10;
+// int ANGLE2 = 20;
+// int ANGLE3 = 30;
+
+// List of flap notches
+int ANGLES[] = { 0, 10, 20, 30, 40, 50 };
+int NOTCH_COUNT = sizeof(ANGLES) / sizeof(ANGLES[0]);
+
 int TITLE_MARGIN = 8;
+int TITLE_HEIGHT = 20;
 int ACTUATOR_SPEED = 255;
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -61,8 +67,6 @@ void setup() {
   pinMode(EXTEND_BUTTON, INPUT_PULLUP);
   pinMode(RETRACT_BUTTON, INPUT_PULLUP);
 
-
-
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -81,19 +85,21 @@ void setup() {
 
   drawTitle();
 
-  drawReferenceLine(ANGLE0);
-  drawReferenceValue(ANGLE0);
+  // drawReferenceLine(ANGLE0);
+  // drawReferenceValue(ANGLE0);
 
 
-  drawReferenceLine(ANGLE1);
-  drawReferenceValue(ANGLE1);
+  // drawReferenceLine(ANGLE1);
+  // drawReferenceValue(ANGLE1);
 
-  drawReferenceLine(ANGLE2);
-  drawReferenceValue(ANGLE2);
+  // drawReferenceLine(ANGLE2);
+  // drawReferenceValue(ANGLE2);
 
-  drawReferenceLine(ANGLE3);
-  drawReferenceValue(ANGLE3);
+  // drawReferenceLine(ANGLE3);
+  // drawReferenceValue(ANGLE3);
 
+  drawReferenceLines();
+  drawReferenceValues();
 
   // display.display();
 
@@ -113,16 +119,29 @@ void loop() {
   }
   
   // int SensorReading = getTestAngleData();
-  int SensorReading = map( analogRead(A2), 10, 870, ANGLE0, ANGLE3 );
+  int SensorReading = map( analogRead(A2), 10, 870, ANGLES[0], ANGLES[NOTCH_COUNT - 1] );
   drawNewAngle(SensorReading);
-  Serial.println(SensorReading, DEC);
-  delay(300);
+  Serial.print("loop() sensorReading: ");Serial.println(SensorReading, DEC);
+  delay(500);
   
 }
 
-void drawReferenceLine(int angle) {
-  
-  display.fillRect(23, getYPosition(angle)+TITLE_MARGIN, 13, 5, SSD1306_WHITE);
+void drawReferenceLines() {
+  int numberOfNotches = sizeof(ANGLES) / sizeof(ANGLES[0]);
+  int screenDistanceBetweenNotches = (SCREEN_WIDTH - (TITLE_MARGIN * 2)) / numberOfNotches;
+
+  // iterate through the defined notch settings
+  for (int i = 0; i < numberOfNotches; i++) {
+    Serial.print(ANGLES[i]); Serial.print(":");
+    drawReferenceLine(ANGLES[i], i, screenDistanceBetweenNotches);
+  }
+  Serial.println();
+}
+
+void drawReferenceLine(int notch, int notchNumber, int distance) {
+  int yPosition = (notchNumber * distance) + TITLE_MARGIN;
+  display.fillRect(23, yPosition + TITLE_MARGIN + 11, 13, 5, SSD1306_WHITE);
+  // display.fillRect(23, getYPosition(angle)+TITLE_MARGIN, 13, 5, SSD1306_WHITE);
   display.display();
 
 }
@@ -141,15 +160,33 @@ void drawTitle() {
 
 }
 
-void drawReferenceValue(int angle) {
+void drawReferenceValues() {
+  // int numberOfNotches = sizeof(ANGLES) / sizeof(ANGLES[0]);
+  int screenDistanceBetweenNotches = (SCREEN_WIDTH - (TITLE_MARGIN * 2)) / NOTCH_COUNT;
 
+  // iterate through the defined notch settings
+  for (int i = 0; i < NOTCH_COUNT; i++) {
+    drawReferenceValue(ANGLES[i], i, screenDistanceBetweenNotches);
+  }
+
+}
+
+void drawReferenceValue(int angle, int notchNumber, int distance) {
+  int yPosition = notchNumber * distance + TITLE_MARGIN;
+  
   char textValues[3];
-  sprintf(textValues, "%d", angle);
+  if (angle < 10) {
+    sprintf(textValues, " %d", angle);
+  } else {
+    sprintf(textValues, "%d", angle);
+  }
+  
 
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(40, getYPosition(angle)+ 3);
+  display.setCursor(40, yPosition + TITLE_MARGIN + 6 );
   display.cp437(true);
+
   display.write(textValues);
   display.display();
 
@@ -157,252 +194,50 @@ void drawReferenceValue(int angle) {
 
 }
 
-
-
 void drawNewAngle(int angle) {
-
-  display.fillRect(0, getYPosition(ANGLE0), 22, SCREEN_WIDTH, SSD1306_BLACK);
-  display.fillRect(2, getYPosition(angle) + TITLE_MARGIN, 18, 5, SSD1306_WHITE);
+  angle = (angle / 2) * 2;
+  // int numberOfNotches = sizeof(ANGLES) / sizeof(ANGLES[0]);
+  int screenDistanceBetweenNotches = (SCREEN_WIDTH - (TITLE_MARGIN * 2)) / NOTCH_COUNT;
+  
+  int yPosition = map(angle, ANGLES[0], ANGLES[NOTCH_COUNT - 1], TITLE_HEIGHT, SCREEN_WIDTH - TITLE_HEIGHT);
+  yPosition = (yPosition / 2) * 2;
+  Serial.print("new angle yPosition: "); Serial.println(yPosition);
+  display.fillRect(0, TITLE_HEIGHT, 22, SCREEN_WIDTH, SSD1306_BLACK);
+  display.fillRect(2, yPosition + TITLE_MARGIN -1, 18, 5, SSD1306_WHITE);
   display.display();
-  delay(10);
 
 }
 
 int getYPosition(int angle) {
-  int newValue = map(angle, 0, ANGLE3 + 5, 20, SCREEN_WIDTH);
-  // Serial.print(angle); Serial.print(": "); Serial.println(newValue);
+  // int newValue = map(angle, 0, ANGLE3 + 5, 20, SCREEN_WIDTH);
+  // int arraySize = sizeof(ANGLES) / sizeof(ANGLES[0]);
+  int newValue = map(angle, ANGLES[0], ANGLES[NOTCH_COUNT - 1], TITLE_HEIGHT, SCREEN_WIDTH);
+  Serial.print("angle:");Serial.print(angle); Serial.print(" yPosition: "); Serial.println(newValue);
   return newValue;
   
 }
 
-int testAngle = 0;
-bool countUp = true;
+// int testAngle = 0;
+// bool countUp = true;
 
-int getTestAngleData() {
+// int getTestAngleData() {
 
-  if (testAngle <= ANGLE1) {
-    countUp = true;
-  }
-  if (testAngle >= ANGLE3) {
-    countUp = false;
-  }
-
-  if (countUp) {
-    testAngle = testAngle + 2;
-  } else {
-    testAngle = testAngle - 2;
-  }
-  delay(100);
-  // Serial.println(testAngle);
-  return testAngle;
-  
-}
-
-// void testdrawchar(void) {
-//   display.clearDisplay();
-
-//   display.setTextSize(1);      // Normal 1:1 pixel scale
-//   display.setTextColor(SSD1306_WHITE); // Draw white text
-//   display.setCursor(0, 0);     // Start at top-left corner
-//   display.cp437(true);         // Use full 256 char 'Code Page 437' font
-
-//   // Not all the characters will fit on the display. This is normal.
-//   // Library will draw what it can and the rest will be clipped.
-//   for(int16_t i=0; i<256; i++) {
-//     if(i == '\n') display.write(' ');
-//     else          display.write(i);
+//   if (testAngle <= ANGLE1) {
+//     countUp = true;
+//   }
+//   if (testAngle >= ANGLE3) {
+//     countUp = false;
 //   }
 
-//   display.display();
-//   delay(2000);
-// }
-
-
-
-// void testdrawrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2; i+=2) {
-//     display.drawRect(i, i, display.width()-2*i, display.height()-2*i, SSD1306_WHITE);
-//     display.display(); // Update screen with each newly-drawn rectangle
-//     delay(1);
+//   if (countUp) {
+//     testAngle = testAngle + 2;
+//   } else {
+//     testAngle = testAngle - 2;
 //   }
-
-//   delay(2000);
-// }
-
-// void testfillrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2; i+=3) {
-//     // The INVERSE color is used so rectangles alternate white/black
-//     display.fillRect(i, i, display.width()-i*2, display.height()-i*2, SSD1306_INVERSE);
-//     display.display(); // Update screen with each newly-drawn rectangle
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-
-// void testdrawroundrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2-2; i+=2) {
-//     display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i,
-//       display.height()/4, SSD1306_WHITE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testfillroundrect(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<display.height()/2-2; i+=2) {
-//     // The INVERSE color is used so round-rects alternate white/black
-//     display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i,
-//       display.height()/4, SSD1306_INVERSE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testdrawtriangle(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=0; i<max(display.width(),display.height())/2; i+=5) {
-//     display.drawTriangle(
-//       display.width()/2  , display.height()/2-i,
-//       display.width()/2-i, display.height()/2+i,
-//       display.width()/2+i, display.height()/2+i, SSD1306_WHITE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-// void testfilltriangle(void) {
-//   display.clearDisplay();
-
-//   for(int16_t i=max(display.width(),display.height())/2; i>0; i-=5) {
-//     // The INVERSE color is used so triangles alternate white/black
-//     display.fillTriangle(
-//       display.width()/2  , display.height()/2-i,
-//       display.width()/2-i, display.height()/2+i,
-//       display.width()/2+i, display.height()/2+i, SSD1306_INVERSE);
-//     display.display();
-//     delay(1);
-//   }
-
-//   delay(2000);
-// }
-
-
-
-// void testdrawstyles(void) {
-//   display.clearDisplay();
-
-//   display.setTextSize(1);             // Normal 1:1 pixel scale
-//   display.setTextColor(SSD1306_WHITE);        // Draw white text
-//   display.setCursor(0,0);             // Start at top-left corner
-//   display.println(F("Hello, world!"));
-
-//   display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-//   display.println(3.141592);
-
-//   display.setTextSize(2);             // Draw 2X-scale text
-//   display.setTextColor(SSD1306_WHITE);
-//   display.print(F("0x")); display.println(0xDEADBEEF, HEX);
-
-//   display.display();
-//   delay(2000);
-// }
-
-// void testscrolltext(void) {
-//   display.clearDisplay();
-
-//   display.setTextSize(2); // Draw 2X-scale text
-//   display.setTextColor(SSD1306_WHITE);
-//   display.setCursor(10, 0);
-//   display.println(F("scroll"));
-//   display.display();      // Show initial text
 //   delay(100);
-
-//   // Scroll in various directions, pausing in-between:
-//   display.startscrollright(0x00, 0x0F);
-//   delay(2000);
-//   display.stopscroll();
-//   delay(1000);
-//   display.startscrollleft(0x00, 0x0F);
-//   delay(2000);
-//   display.stopscroll();
-//   delay(1000);
-//   display.startscrolldiagright(0x00, 0x07);
-//   delay(2000);
-//   display.startscrolldiagleft(0x00, 0x07);
-//   delay(2000);
-//   display.stopscroll();
-//   delay(1000);
+//   // Serial.println(testAngle);
+//   return testAngle;
+  
 // }
 
-// void testdrawbitmap(void) {
-//   display.clearDisplay();
 
-//   display.drawBitmap(
-//     (display.width()  - LOGO_WIDTH ) / 2,
-//     (display.height() - LOGO_HEIGHT) / 2,
-//     logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
-//   display.display();
-//   delay(1000);
-// }
-
-// #define XPOS   0 // Indexes into the 'icons' array in function below
-// #define YPOS   1
-// #define DELTAY 2
-
-// void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
-//   int8_t f, icons[NUMFLAKES][3];
-
-//   // Initialize 'snowflake' positions
-//   for(f=0; f< NUMFLAKES; f++) {
-//     icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-//     icons[f][YPOS]   = -LOGO_HEIGHT;
-//     icons[f][DELTAY] = random(1, 6);
-//     Serial.print(F("x: "));
-//     Serial.print(icons[f][XPOS], DEC);
-//     Serial.print(F(" y: "));
-//     Serial.print(icons[f][YPOS], DEC);
-//     Serial.print(F(" dy: "));
-//     Serial.println(icons[f][DELTAY], DEC);
-//   }
-
-//   for(;;) { // Loop forever...
-//     display.clearDisplay(); // Clear the display buffer
-
-//     // Draw each snowflake:
-//     for(f=0; f< NUMFLAKES; f++) {
-//       display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, SSD1306_WHITE);
-//     }
-
-//     display.display(); // Show the display buffer on the screen
-//     delay(200);        // Pause for 1/10 second
-
-//     // Then update coordinates of each flake...
-//     for(f=0; f< NUMFLAKES; f++) {
-//       icons[f][YPOS] += icons[f][DELTAY];
-//       // If snowflake is off the bottom of the screen...
-//       if (icons[f][YPOS] >= display.height()) {
-//         // Reinitialize to a random position, just off the top
-//         icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-//         icons[f][YPOS]   = -LOGO_HEIGHT;
-//         icons[f][DELTAY] = random(1, 6);
-//       }
-//     }
-//   }
-// }
